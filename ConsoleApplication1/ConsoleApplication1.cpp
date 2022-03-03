86e15bab652b518db106b88c7385f5cc7b928d39
Evissu <blessedereleven@gmail.com>
2022-03-03 20:53:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	//cout << "Name           :\"The warand the peace\"\nAvtor          :L.N.Tolstoj\nIzdatelstvo    :Piter\nPages          :500";
+	cout << "\"\a\a\aU lukomor'a dub zelenij,\n\a\a\aZlataya zep na dupe tom,\n\a\a\aI dnem i nochju kot uchenij,\n\a\a\avse hodit po cepi krugom";
+}
+//тип_переменной имя_переменной;
+/*int a, er, ds; // int a,c
+	float b = 4.02;
+	int c = 60;
+	a = 15;*/
+	/*
+	string str = "Hello \"World!\"\n";
+	char ch = '$'; // символьная переменная char
+
+	cout << str << endl;
+	cout << "Hello World\n";
+	*/ //setlocale(LC_ALL, ""); ПОЗВОЛЯЕТ ОТОБРАЖАТЬ РУССКИЙ ЯЗЫК В КОИАНДНОЙ СТРОКЕ
+
+
+
+	//cin >> a; ввод какого либо элемента
+	/*
+	\n - табуляция новая строка
+	\t - горизонтальная табуляция
+	\v - вертикальная табуляция
+	\r - возврат коретки
+	\0 - символ концав строки
+	\b - возврат на одну позицию
+	\a - звуковой сигнал
+	\
+	*/
+	/*
+	int a = 15;
+	int b = 2;
+	float c = (float)a / (float)b;
+	
+	cout << c;
+	*/
+
